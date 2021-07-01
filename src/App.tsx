9814f20bc91a37be8cd24b2d8d703ecfc8a6171d
epam_Nikiforov <Aleksandr_Nikiforov@epam.com>
2021-07-01 18:03:23 +0400

    added types/react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,54 @@
 import React from 'react';
-import './styles.scss';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from './features/counter/counter-slice';
+import { useFetchMoviesQuery } from './features/movies/movies-api-slice';
+import './App.css';
 
-console.log('process', process.env.KEY, process.env.PORT);
+function App(): JSX.Element {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
 
-const App: React.FC = () => {
+  const { data, isFetching } = useFetchMoviesQuery();
+
+  function handleInk() {
+    dispatch(increment());
+  }
+
+  function handleDec() {
+    dispatch(decrement());
+  }
+
+  function handleAmountAdded() {
+    dispatch(incrementByAmount(3));
+  }
+
+  if (isFetching) {
+    return <div>Fetching data...</div>;
+  }
+
+  console.log('data', data.total, data.limit, data.data);
+
   return (
-    <div className="wrapper">
-      <h1>React 17 and TypeScript 4 App!🚀</h1>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <button type="button" onClick={handleInk}>
+            INC. count is: {count}
+          </button>
+          <button type="button" onClick={handleDec}>
+            DEC. count is: {count}
+          </button>
+          <button type="button" onClick={handleAmountAdded}>
+            AmountAdded. count is: {count}
+          </button>
+        </p>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
